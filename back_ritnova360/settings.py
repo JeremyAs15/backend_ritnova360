fe00ffdd0fcea12603c92c86e8d0a95033c8729a
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2026-05-24 18:44:59 -0500

    minor fix

--- back_ritnova360/settings.py
+++ back_ritnova360/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = [
     'localhost', 
     '127.0.0.1', 
     '10.0.2.2', 
-    'backend-ritnova360.onrender.com'  # <--- Agrega exactamente esta URL
+    'backend-ritnova360.onrender.com'  
 ]
 
 
