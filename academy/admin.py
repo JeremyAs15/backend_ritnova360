69d3118a1fa73d53a3c62f3d650cd6d8496a1934
Santiago Guerrero <santiago.david.guerrero@correounivalle.edu.co>
2026-05-30 17:59:32 -0500

    personalization of admin edition

--- academy/admin.py
+++ academy/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from .models import Choreography, VideoClip, Rate, Enroll, ShoppingCart, AddTo
 
-# Register your models here.
+class VideoClipInline(admin.TabularInline):
+    """
+    Permite agregar o editar videoclips directamente desde el formulario de la coreografía.
+    """
+    model = VideoClip
+    extra = 1  # Espacios vacíos por defecto para nuevos clips
+
+
+class AddToInline(admin.TabularInline):
+    """
+    Muestra los ítems añadidos a un carrito de compras.
+    """
+    model = AddTo
+    extra = 0
+    readonly_fields = ('price_at_purchase',)
+
+
+@admin.register(Choreography)
+class ChoreographyAdmin(admin.ModelAdmin):
+    list_display = ('song_name', 'genre', 'difficulty_level', 'price', 'creator', 'creation_date')
+    list_filter = ('genre', 'difficulty_level', 'creation_date')
+    search_fields = ('song_name', 'genre', 'creator__email')
+    inlines = [VideoClipInline]
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('shopping_cart_id', 'user', 'state', 'date')
+    list_filter = ('state', 'date')
+    search_fields = ('user__email', 'shopping_cart_id')
+    inlines = [AddToInline]
+
+
+@admin.register(Enroll)
+class EnrollAdmin(admin.ModelAdmin):
+    list_display = ('user', 'choreography', 'date', 'state', 'id_source')
+    list_filter = ('state', 'date')
+    search_fields = ('user__email', 'choreography__song_name')
+
+
+@admin.register(Rate)
+class RateAdmin(admin.ModelAdmin):
+    list_display = ('user', 'choreography', 'score', 'date')
+    list_filter = ('score', 'date')
+    search_fields = ('user__email', 'choreography__song_name')
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    """
+    Personalización del panel de administración para el modelo de usuario personalizado.
+    """
+    ordering = ('email',)
+    list_display = ('email', 'first_name', 'last_name', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active', 'city')
+    search_fields = ('email', 'first_name', 'last_name', 'n_documento')
+    
+    # Organización de los campos dentro del formulario de edición
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Información Personal', {
+            'fields': ('first_name', 'last_name', 'phone_number', 'document_type', 'n_documento', 'birth_date', 'genre')
+        }),
+        ('Ubicación', {
+            'fields': ('country', 'department', 'city')
+        }),
+        ('Roles y Permisos', {
+            'fields': ('role', 'teacher_type', 'is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+        ('Facturación', {
+            'fields': ('datos_facturacion_default',)
+        }),
+        ('Fechas Importantes', {
+            'fields': ('last_login', 'date_joined')
+        }),
+    )
+    
+    # Campos al crear un usuario nuevo
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password', 'role', 'is_staff', 'is_active'),
+        }),
+    )
